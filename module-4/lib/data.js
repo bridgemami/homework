14c3e9b51573153fe473aa21b95272e22f01fbde
bridgemami <mdbridgeman@gmail.com>
2022-09-13 10:55:45 -0700

    changed author.json. got rid of height and width.

--- module-4/lib/data.js
+++ module-4/lib/data.js
@@ -30,6 +30,26 @@ export function getSortedList() {
 }
 
 //async function to get the complete data for just one author/person
+// used by getStaticProps() in [id].js
 export async function getData(idRequested) {
-
+// get filepath to data directory
+// const dataJ = path.join( process.cwd(), 'data');
+//load json file contents
+const jsonData = fs.readFileSync( filePath, 'utf8' );
+//convert string from file into json array object
+const jsonObj = JSON.parse( jsonData);
+//find object value in array that has matching id, return an array with one element
+const objMatch = jsonObj.filter(obj => {
+   return obj.id.toString() === idRequested; 
+}
+);
+//extract object value in filtered array if any
+let objReturned;
+if(objMatch.length > 0) {
+    objReturned = objMatch[0];
+}
+else {
+    objReturned = {};
+}
+return objReturned;
 }
--- module-4/pages/[id].js
+++ module-4/pages/[id].js
@@ -1,5 +1,33 @@
 import Head from 'next/head';
 //getAllIds is a function from lib/data.js
-import { getAllIds } from '../lib/data';
+import { getAllIds, getData } from '../lib/data';
+
+//create an instance of the getStaticProps() to return data for one person
+export async function getStaticProps({params}) {
+    const itemData = await getData(params.id);
+    return  {
+        props: {
+            itemData
+        }
+    }
+}
 
 //create an instance of the getStaticPaths() to report next all possible dynamic urls
+export async function getStaticPaths() {
+const allDynamicPaths = getAllIds();
+return {
+    allDynamicPaths,
+    fallback: false
+};
+}
+
+// make a react component to display all details about a person when a dynamic route matches, like id 1 or id 2
+export default function Entry ({itemData}) {
+    <article class= "card col-4">
+        <div class="card-body">
+            <h5 class="card-title">
+               {itemData.author} </h5>
+               <h6 class="card-subtitle mb-3 text-muted">{itemData.quote}</h6>
+            </div>
+        </article>
+}
